877cb9ab7a943492092ca7d2600777b4da42a2e0
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-15 12:11:25 -0400

    added likes control

---
+++ fields/automate-likes.php
@@ -0,0 +1,1 @@
+<input style="width: 100%; border: 1px solid #069; padding: 0.75em; border-radius: 5px; color: #069;" type="text" name="automate_likes" id="automate_likes" likes="<?php echo get_option('automate_likes'); ?>" />
