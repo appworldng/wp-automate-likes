801892c2fb4dc0b4f1e03852acb3cccd7ccfef76
chigozieorunta <chigozieorunta@yahoo.com>
2020-05-15 12:11:08 -0400

    added increment control

---
+++ fields/automate-increment.php
@@ -0,0 +1,1 @@
+<input style="width: 100%; border: 1px solid #069; padding: 0.75em; border-radius: 5px; color: #069;" type="text" name="automate_increment" id="automate_increment" value="<?php echo get_option('automate_increment'); ?>" />
